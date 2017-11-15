13c157414bbe45d39e55596e4606d6a16be92f7e
jason19956779 <jason199567@gmail.com>
2017-11-16 01:30:29 +0800

    Bug fixes and some UI improvements
    1)Fix no proper output when longitude is 90W/E.
    2)Let the output data type all numbers.
    3)Improve UI for better UX.
    4)Add dependent output precision variable.
    5)Slightly improve calculation process.
    6)Add a better test case.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var GnoProj = require('./src/gnomonic')
 var readlineSync = require('readline-sync')
-
+var prcin = 4 //customisable digit precision, GnoProj.project's default precision is 4.
+prcin.toPrecision
 console.log('A Gnomonic Projection Application.')
 console.log('Please Enter Latitude and Longitude.\n')
 console.log('Range:\nLatitude between 0° and 90°N\nLongitude between 0° and 180°')
-console.log('eg. Latitude: 60\n    Longitude: 120E\n')
+console.log('ex. Latitude: 23.5\n    Longitude: 121E\n')
 
+console.log('Input:')
 var latitude = readlineSync.question('Latitude: ')
 if(latitude>=0 && latitude <=90){
     console.log(`${latitude}, checked.`)
@@ -28,5 +30,6 @@ else{
     process.exit()
 }
 
+console.log('\nOutput:')
 var coordinate = GnoProj.project(latitude, longitude)
 console.log(`Cartesian Coordinate:\nx:${coordinate.x}\ny:${coordinate.y}`)
--- src/gnomonic.js
+++ src/gnomonic.js
@@ -2,12 +2,13 @@ var GnoProj = module.exports = {}
 var math = require('mathjs')
 var R = 15
 var r = 0
+var prcin = 4
 
 GnoProj.project = function(latitude, longitude) {
     var coordinate = {x:0, y:0}
 
     if(latitude>0 && latitude<90){
-        r = R*(math.cot((math.pi/2)*(latitude/90)))
+        r = R*(math.cot((math.pi)*(latitude/180)))
     }
     else if(latitude == 0){
         coordinate.x = 'Not Applicable (is Equator).'
@@ -21,28 +22,40 @@ GnoProj.project = function(latitude, longitude) {
     }
 
     var angle = (/\d+/.exec(longitude))[0]
+    var hemisphere = ''
 
-    if(angle>0 && angle<180 && angle!==90){
-        coordinate.x = r*(math.abs(math.sin((math.pi/2)*(angle/90))))
-        coordinate.y = r*(math.abs(math.cos((math.pi/2)*(angle/90))))
-        var hemisphere = (/\D/.exec(longitude))[0]
-        if(hemisphere == 'W'){
-            coordinate.x=-coordinate.x
+    if(angle>0 && angle<180 && angle!=90){
+        hemisphere = (/\D/.exec(longitude))[0]
+        if(hemisphere == 'W' || hemisphere == 'E')
+        {
+            coordinate.x = r*(math.abs(math.sin((math.pi)*(angle/180))))
+            coordinate.y = r*(math.abs(math.cos((math.pi)*(angle/180))))
+            if(hemisphere == 'W'){
+                coordinate.x=-coordinate.x
+            }
+            if(angle<90){
+                coordinate.y=-coordinate.y
+            }
         }
-        if(angle<90){
-            coordinate.y=-coordinate.y
+        else{
+            console.log('Something went wrong.')
+            process.exit()
         }
-        return coordinate
     }
-    else if(angle == 0){
+    else if(angle == 0 || angle == 180){
         coordinate.x = 0
-        coordinate.y = -r
-        return coordinate
+        coordinate.y = (angle == 0)?-r:r
     }
-    else if(angle == 180){
-        coordinate.x = 0
-        coordinate.y = r
-        return coordinate
+    else if(angle == 90){
+        hemisphere = (/\D/.exec(longitude))[0]
+        coordinate.x = (hemisphere == 'W')?-r:r
+        coordinate.y = 0
     }
-
+    else{
+        console.log('Something went wrong.')
+        process.exit()
+    }
+    coordinate.x = Number( (coordinate.x == 0)?0:((coordinate.x).toPrecision(prcin)) )
+    coordinate.y = Number( (coordinate.y == 0)?0:((coordinate.y).toPrecision(prcin)) )
+    return coordinate
 }
